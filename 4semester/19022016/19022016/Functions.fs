355be4d1e945132c9b8c90cda8ddd9a3acdca085
likeanowl <sum6969@yandex.ru>
2016-02-25 20:08:36 +0300

    1st homework 4 sem

---
+++ 4semester/19022016/19022016/Functions.fs
@@ -0,0 +1,26 @@
+﻿module Functions
+
+/// Factorial
+let rec factorial x =
+    if x = 0 then 1 else x * factorial (x - 1)
+
+/// Fibbonachi numbers
+let rec fibbonacci x = 
+    if x = 1 || x = 0 then 1 else fibbonacci (x - 2) + fibbonacci (x - 1)
+
+/// Reverses list
+let reverseList list = 
+    let rec recursiveReverse list tempList =
+        match list with
+        | [] -> tempList
+        | head :: tail -> recursiveReverse tail (head :: tempList)
+    recursiveReverse list []
+
+/// Ivolutes 2 in power
+let rec power pow = 
+    if pow = 0 then 1 else 2 * power (pow - 1)
+
+/// Creates consequence of 2 pow's 
+let powersOfTwo startPow =
+    List.init 5 (fun i -> power (startPow + i))
+
---
+++ 4semester/19022016/19022016/Program.fs
@@ -0,0 +1,12 @@
+﻿open Functions
+
+[<EntryPoint>]
+
+let main args = 
+    printfn "factorial of %d = %d" 5 (factorial 5)
+    printfn "4th fibbonacci number is %d" (fibbonacci 4)
+    let list = [1; 2; 3; 4; 5]
+    printfn "default list : %O" list
+    printfn "reversed list : %O" (reverseList list)
+    printfn "list of 2 pow's from 5 : %O" (powersOfTwo 5)
+    0
