8880a6bdc99702138cd5c3f7a5492af97b5531e4
likeanowl <sum6969@yandex.ru>
2016-03-03 07:27:06 +0300

    Second task

---
+++ 4semester/26022016/26022016/Functions.fs
@@ -0,0 +1,39 @@
+﻿module Functions 
+
+/// Digitals multiplication
+let rec dMult n = 
+    if n < 10 then n else (n % 10) * dMult (n / 10)
+
+/// Index in list
+let rec indVarInList list var =
+    let rec innerRec ls v ind =
+        match ls with
+        | [] -> -1
+        | h :: t -> if v = h then ind else innerRec t v (ind + 1)
+    innerRec list var 0
+
+/// Reverses list (helper function)
+let reverseList list = 
+    let rec recursiveReverse list tempList =
+        match list with
+        | [] -> tempList
+        | head :: tail -> recursiveReverse tail (head :: tempList)
+    recursiveReverse list []
+
+/// Checking is string a palindrom
+let pal str =
+    let list = [for i in str -> i]
+    let removeLast list = reverseList (reverseList list).Tail
+    let rec innerRec list =
+        match list with 
+        | [] -> true
+        | h :: [] -> true
+        | h :: t -> if h = (reverseList t).Head then innerRec (removeLast t) else false
+    innerRec list
+
+/// Cheking is all elemets are different
+let rec isAllDifferent list =
+    match list with
+    | [] -> false
+    | h :: [] -> true
+    | h :: t -> if h = t.Head then false else isAllDifferent t 
---
+++ 4semester/26022016/26022016/Program.fs
@@ -0,0 +1,15 @@
+﻿open Functions
+
+// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+[<EntryPoint>]
+let main argv = 
+    printfn "Multiplication of all digits in %d = %d" 0 (dMult 0)
+    printfn "Multiplication of all digits in %d = %d" 10 (dMult 10)
+    printfn "Multiplication of all digits in %d = %d" 1234 (dMult 1234)
+    let list = [1; 2; 3; 4; 5]
+    printfn "Index of %d in %O is : %d" 3 list (indVarInList list 3)
+    let str = "abcda"
+    printfn "Is %s a palindrom? %b" str (pal str)
+    printfn "Is all elements of %O are different? %b" list (isAllDifferent list)
+    0
