a26faa42e0e007fed82ce4c3c514504e51568a23
Sergey Svitkov <sum6969@yandex.ru>
2016-03-06 22:23:21 +0300

    Fixed 3, 4 tasks

--- 4semester/26022016/26022016/Functions.fs
+++ 4semester/26022016/26022016/Functions.fs
@@ -23,17 +23,22 @@ let reverseList list =
 /// Checking is string a palindrom
 let pal str =
     let list = [for i in str -> i]
-    let removeLast list = reverseList (reverseList list).Tail
     let rec innerRec list =
         match list with 
         | [] -> true
         | h :: [] -> true
-        | h :: t -> if h = (reverseList t).Head then innerRec (removeLast t) else false
+        | h :: t -> if h = (reverseList t).Head then innerRec (reverseList t).Tail else false
     innerRec list
 
 /// Cheking is all elemets are different
 let rec isAllDifferent list =
+    let rec matchWith list var =
+        match list with
+        | [] -> false
+        | h :: t -> if h = var then true else matchWith t var
     match list with
     | [] -> false
     | h :: [] -> true
-    | h :: t -> if h = t.Head then false else isAllDifferent t 
+    | h :: t -> if h = t.Head then false 
+                else if matchWith t h then false 
+                else isAllDifferent t 
