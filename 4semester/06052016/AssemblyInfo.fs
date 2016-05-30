a1715919b299cbde3be7a6ae5c7a0e2ccc645a23
likeanowl <sum6969@yandex.ru>
2016-05-31 00:56:45 +0300

    8

---
+++ 4semester/06052016/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿module _06052016.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("06052016")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("likeanowl")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
